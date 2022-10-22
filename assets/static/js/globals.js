fce4d7a19389ff83cf245ce8e906598508e42a57
Yubo-Cao <cao2006721@gmail.com>
2022-10-22 10:51:03 -0400

    feat: JavaScript frontend part.

--- assets/static/js/globals.js
+++ assets/static/js/globals.js
@@ -1,7 +1,7 @@
 var __NAME__ = "Traveler";
 var __VERSION__ = "0.0.1";
 var __AUTHOR__ = "Anish Goyal, Celine Cheriyankandath, Yubo Cao";
-var __DESCRIPTION__ = "Traveler";
+var __DESCRIPTION__ = "A travel app that recommends users stuff about travel yada yada yada";
 var __LICENSE__ = "MIT";
 var __KEYWORDS__ = "Travel";
 
--- assets/static/js/signin_side.js
+++ assets/static/js/signin_side.js
@@ -12,13 +12,14 @@ import Grid from '@material-ui/core/Grid';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+import * as global from 'globals.js';
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
       <Link color="inherit" href="https://mui.com/">
-        Your Website
+        global.__NAME__
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
