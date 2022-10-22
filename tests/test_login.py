0679df12ab24d83ea784bf0fa31c576d2faa7751
Yubo-Cao <cao2006721@gmail.com>
2022-10-22 12:26:39 -0400

    feat: add more test cases for the new feature

--- tests/test_login.py
+++ tests/test_login.py
@@ -7,6 +7,11 @@ URL = "http://127.0.0.1:5000"
 
 
 def test_register():
+    try:
+        requests.post(URL + "/unregister", data={"username": USERNAME})
+    except requests.exceptions.ConnectionError:
+        pass
+
     response = requests.post(
         URL + "/register", data={"username": USERNAME, "password": PASSWORD}
     )
