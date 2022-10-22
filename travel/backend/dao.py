6eab8e10446b1b93598c5bc68d6be93e66213341
Yubo-Cao <cao2006721@gmail.com>
2022-10-22 14:20:36 -0400

    feat: dao in progress.

--- travel/backend/dao.py
+++ travel/backend/dao.py
@@ -1,30 +1,100 @@
 import inspect
 import os
+from collections.abc import Callable
+from typing import TypeVar
 
+import requests
 from flask import jsonify
 
 from travel.backend.db import DBSession, Event, Level, Thread, User
 
+T = TypeVar("T")
 
-def ensure_login(fn):
+
+def ensure_login(fn: Callable[[User], T]) -> Callable[[str, str], T]:
     # ensure password and username parameters
-    sign = inspect.signature(fn)
-    if "password" not in sign.parameters:
-        raise Exception("password parameter is required")
-    if "username" not in sign.parameters:
-        raise Exception("username parameter is required")
 
-    def wrapper(*args, **kwargs):
-        ba = sign.bind(*args, **kwargs)
-        username = ba.arguments["username"]
-        password = ba.arguments["password"]
-
+    def wrapper(password, username):
         with DBSession() as session:
             user = session.query(User).filter_by(username=username).first()
             if user is None:
                 return jsonify({"message": "User does not exist."})
             if user.password != password:
                 return jsonify({"message": "Incorrect password."})
-        return fn(*args, **kwargs)
+        return fn(user)
 
     return wrapper
+
+
+@ensure_login
+def profile(user):
+    """
+    Return: {
+        "level": level (int),
+        "events": event (list[Event]),
+        "streak": streak (int)
+        "percent_category": percent_category (dict[Category: float])
+        "XP": xp (int) (number_of_city_visited * 10 + number_of_events * 5)
+        "number_of_city_visited": number (int),
+        "number_of_country_visited": number (int),
+        "number_of_events": number (int),
+        "number_of_threads": number (int),
+    }
+    """
+
+    with DBSession() as session:
+        events = session.query(Event).filter_by(user=user).all()
+        threads = session.query(Thread).filter_by(user=user).all()
+        number_of_city_visited = len(
+            set(event.location.split(", ", 2)[0] for event in events)
+        )
+        number_of_country_visited = len(
+            set(
+                event.location.split(", ", 2)[-1] for event in events
+            )  # location: "city, country"
+        )
+        number_of_events = len(events)
+        number_of_threads = len(threads)
+        xp = number_of_city_visited * 10 + number_of_events * 5
+        streak = 0
+        events = sorted(events, key=lambda event: event.date)
+        for i in range(len(events) - 1):
+            if events[i + 1].date - events[i].date == datetime.timedelta(days=1):
+                streak += 1
+            else:
+                break
+
+        percent_category = {}
+        return jsonify(
+            {
+                "level": user.level.value,
+                "events": [event.as_dict() for event in events],
+                "streak": streak,
+                "percent_category": percent_category,
+                "XP": xp,
+                "number_of_city_visited": number_of_city_visited,
+                "number_of_country_visited": number_of_country_visited,
+                "number_of_events": number_of_events,
+                "number_of_threads": number_of_threads,
+            }
+        )
+
+
+@ensure_login
+def get_recommendation(user):
+    pass
+
+
+def get_common_places(city: str):
+    """
+    Calling google map API to get common places
+    """
+
+    url = "https://maps.googleapis.com/maps/api/place/textsearch/json"
+    params = {
+        "query": city,
+        "key": os.environ["GOOGLE_MAPS_API_KEY"],
+    }
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    return response.json()
